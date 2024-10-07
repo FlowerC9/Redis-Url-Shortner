cac40c892601e67622a541ba4a7c354b9438803a
Monojit Tewari <tewarimonojit@gmail.com>
2024-10-07 17:10:18 +0530

    vite config updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    open:true,
-    port:3000
+  server: {
+    open: true,
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',  // Ensure the build output directory is set to 'dist'
   }
 })
